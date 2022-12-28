9ed46e59f33935d474df80e87b0ac01f4fde9ba1
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-28 02:52:42 +0100

    commit zero

---
+++ Madcity Script pastebin roblox Mad city autofarm Script .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet('https://pastebin.com/raw/YB4hsG56'))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							This script have server hop,and you don't need rejoin game
+						
+]]
